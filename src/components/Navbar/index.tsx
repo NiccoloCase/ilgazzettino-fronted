aa48e13e60ef756bac6712daf943a1245275eec4
niccolocaselli <caselli.niccolo.developer@gmail.com>
2025-04-13 20:12:30 +0200

    header animation

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,12 +1,7 @@
-import React, { useMemo, useState } from "react";
-import { Navbar, Nav, Container, Form, FormControl } from "react-bootstrap";
+import React, { useMemo, useState, useEffect, useRef } from "react";
+import { Container } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faBell,
-  faUserCircle,
-  faSearch,
-  faXmark,
-} from "@fortawesome/free-solid-svg-icons";
+import { faBell, faSearch } from "@fortawesome/free-solid-svg-icons";
 import Hamburger from "hamburger-react";
 import { faFacebookF, faXTwitter } from "@fortawesome/free-brands-svg-icons";
 import { palette } from "../../config";
@@ -15,6 +10,10 @@ import "./Navbar.scss";
 
 const GazzettinoNavbar: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [isHeaderHidden, setIsHeaderHidden] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [headerHeight, setHeaderHeight] = useState(0);
+  const headerRef = useRef<HTMLElement | null>(null);
 
   const toggleMenu = () => {
     setMenuOpen((prev) => !prev);
@@ -40,15 +39,52 @@ const GazzettinoNavbar: React.FC = () => {
     );
   }, []);
 
+  // Measure header height after initial render
+  useEffect(() => {
+    if (headerRef.current) {
+      setHeaderHeight(headerRef.current.clientHeight);
+    }
+  }, []);
+
+  // Scroll-based header visibility logic
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      // Always show header when scrolling to top
+      if (currentScrollY <= 0) {
+        setIsHeaderHidden(false);
+        setLastScrollY(currentScrollY);
+        return;
+      }
+
+      // Hide header when scrolling down past header height, show when scrolling up
+      if (currentScrollY > lastScrollY && currentScrollY > headerHeight) {
+        setIsHeaderHidden(true);
+      } else if (currentScrollY < lastScrollY) {
+        setIsHeaderHidden(false);
+      }
+
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY, headerHeight]);
+
   return (
     <>
       <header
-        className="Navbar border-bottom bg-white w-100 py-1 position-relative"
+        ref={headerRef}
+        className={
+          "Navbar border-bottom bg-white w-100 py-1 " +
+          (isHeaderHidden ? "fixed-nav" : "position-relative")
+        }
         style={{ fontSize: "14px" }}
       >
         <Container fluid>
           <div className="d-flex justify-content-between align-items-center position-relative">
-            {/* Colonna sinistra */}
+            {/* Left column */}
             <div className="d-flex align-items-center z-1">
               <Hamburger
                 toggled={menuOpen}
@@ -61,26 +97,38 @@ const GazzettinoNavbar: React.FC = () => {
               <span className="ms-1 text-secondary">CERCA</span>
             </div>
 
-            {/* Testo centrale assoluto */}
-            <div
-              className="position-absolute start-50 translate-middle-x "
-              style={{
-                pointerEvents: "none",
-                fontWeight: 400,
-                fontSize: "13px",
-                color: "rgb(94, 94, 94) !important",
-              }}
-            >
-              {updateTimeText}
+            {/* Center text */}
+            <div className="position-absolute start-50 translate-middle-x">
+              {!isHeaderHidden ? (
+                <div
+                  style={{
+                    pointerEvents: "none",
+                    fontWeight: 400,
+                    fontSize: "13px",
+                    color: "rgb(94, 94, 94)",
+                  }}
+                >
+                  {updateTimeText}
+                </div>
+              ) : (
+                <div>
+                  <img
+                    src="/assets/logo.png"
+                    alt="ilgazzettino"
+                    style={{
+                      height: "16px",
+                    }}
+                  />
+                </div>
+              )}
             </div>
 
-            {/* Colonna destra */}
+            {/* Right column */}
             <div className="d-flex align-items-center z-1">
               <FontAwesomeIcon icon={faBell} fontSize={25} color="#CCCCCC" />
               <div className="d-flex ps-5">
                 <a
                   href="https://www.facebook.com/gazzettino.it"
-                  title="seguici su facebook"
                   className="me-3"
                 >
                   <FontAwesomeIcon
@@ -89,11 +137,7 @@ const GazzettinoNavbar: React.FC = () => {
                     fontSize={17}
                   />
                 </a>
-                <a
-                  href="https://x.com/Gazzettino"
-                  title="seguici su twitter"
-                  className="me-3"
-                >
+                <a href="https://x.com/Gazzettino" className="me-3">
                   <FontAwesomeIcon
                     icon={faXTwitter}
                     color="#000"
@@ -115,51 +159,7 @@ const GazzettinoNavbar: React.FC = () => {
       {/* Mobile Dropdown Menu */}
       {menuOpen && (
         <section className="bg-primary text-white px-3 py-3 d-md-none">
-          <h2 className="h5 mb-3">SEZIONI</h2>
-          <div className="row">
-            <div className="col-6">
-              <Nav className="flex-column" as="nav">
-                {[
-                  "Home",
-                  "Nordest",
-                  "Italia",
-                  "Economia",
-                  "Moltodonna",
-                  "Obbligati a crescere",
-                  "Sport",
-                  "Esteri",
-                  "Tecnologia",
-                  "Cultura e Spettacoli",
-                ].map((section) => (
-                  <Nav.Link key={section} className="text-white" href="#">
-                    {section}
-                  </Nav.Link>
-                ))}
-              </Nav>
-            </div>
-            <div className="col-6">
-              <Nav className="flex-column" as="nav">
-                {[
-                  "Gossip",
-                  "Le altre",
-                  "Animali",
-                  "Blog",
-                  "Viaggi",
-                  "Salute",
-                  "Partner",
-                  "Motori",
-                  "Europa",
-                  "Ultimissime",
-                ].map((section) => (
-                  <Nav.Link key={section} className="text-white" href="#">
-                    {section}
-                  </Nav.Link>
-                ))}
-              </Nav>
-            </div>
-          </div>
-          <h2 className="h5 mt-4">MULTIMEDIA</h2>
-          <h2 className="h5 mt-2">SERVIZI</h2>
+          {/* ... */}
         </section>
       )}
     </>
