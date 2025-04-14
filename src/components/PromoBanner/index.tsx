204fd47e215633a6973b3a4c3cb0a5cfb3c32a58
niccolocaselli <caselli.niccolo.developer@gmail.com>
2025-04-14 14:20:02 +0200

    semantic html for PromoBanner and improved accesibility

--- src/components/PromoBanner/index.tsx
+++ src/components/PromoBanner/index.tsx
@@ -5,13 +5,18 @@ import { useState } from "react";
 import { enqueueSnackbar } from "notistack";
 
 export const PromoBanner = () => {
-  const [isVisible, setIsVisible] = useState(true);
+  const [isVisible, setIsVisible] = useState(
+    // TODO: before production set to true
+
+    false
+  );
 
   const subscribe = () => {
     enqueueSnackbar("Grazie per esserti iscritto alla nostra newsletter!", {
       variant: "success",
       autoHideDuration: 3000,
     });
+    handleClose();
   };
 
   const handleClose = () => {
@@ -21,37 +26,42 @@ export const PromoBanner = () => {
   if (!isVisible) return null;
 
   return (
-    <div
-      className="PromoBanner fixed-bottom d-flex justify-content-between align-items-center"
+    <aside
+      className="PromoBanner fixed-bottom"
+      aria-label="Special promotion"
       style={{
         background:
           "linear-gradient(45deg, rgb(253, 204, 3) 0px, rgb(253, 204, 3) 100%)",
       }}
     >
-      <div>
-        <h3>
-          <strong>Promo 3 Mesi a 9€</strong>
-        </h3>
-        <h5>Per te l'Edizione Digitale e tutto il Sito</h5>
-      </div>
+      <div className="promo-content d-flex justify-content-between align-items-center">
+        <section aria-labelledby="promo-heading promo-subheading">
+          <h2 id="promo-heading" className="h3">
+            <strong>Promo 3 Mesi a 9€</strong>
+          </h2>
+          <p id="promo-subheading" className="h5">
+            Per te l'Edizione Digitale e tutto il Sito
+          </p>
+        </section>
 
-      <div className="d-flex flex-column justify-content-center align-items-end">
-        <button
-          className="close-button"
-          onClick={handleClose}
-          aria-label="Close"
-        >
-          <FontAwesomeIcon icon={faX} size="lg" />
-        </button>
+        <div className="promo-actions d-flex flex-column justify-content-center align-items-end">
+          <button
+            className="close-button"
+            onClick={handleClose}
+            aria-label="Close promotion banner"
+          >
+            <FontAwesomeIcon icon={faX} size="lg" aria-hidden="true" />
+          </button>
 
-        <button
-          className="subscribe-button"
-          onClick={subscribe}
-          aria-label="Subscribe"
-        >
-          <strong>ABBONATI ORA</strong>
-        </button>
+          <button
+            className="subscribe-button"
+            onClick={subscribe}
+            aria-label="Subscribe to newsletter"
+          >
+            <strong>ABBONATI ORA</strong>
+          </button>
+        </div>
       </div>
-    </div>
+    </aside>
   );
 };
