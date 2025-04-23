6546ef462d2692bc1e834ce29d2c357d66c68d0e
niccolocaselli <caselli.niccolo.developer@gmail.com>
2025-04-23 15:11:44 +0200

    not working flex wrap

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -26,34 +26,40 @@ export const HomePage: React.FC = () => {
       <div className="text-center small  border-top border-bottom py-0 mt-1 d-none d-md-block">
         <span style={{ color: "#000", marginRight: "1rem" }}>Temi caldi:</span>
         {TOPICS.map((t, i) => (
-          <a href="#" key={i} className="mx-1 text-decoration-none link">
+          <a href="#" key={i} className="mx-1 hot-link hover-decoration">
             {t}
           </a>
         ))}
       </div>
 
-      <nav className="nav justify-content-center border-bottom py-2 d-none d-md-flex">
+      <nav className="nav justify-content-center border-bottom py-2 d-none d-md-flex ">
         {SECTIONS.map((s, i) => (
-          <a href="#" key={i} className="nav-link px-2 text-dark">
+          <a
+            href="#"
+            key={i}
+            className="nav-link px-2 text-dark hover-decoration"
+          >
             <small>{s}</small>
           </a>
         ))}
       </nav>
 
       <div
-        className=" text-white text-center py-2 d-none d-md-block"
+        className=" text-white text-center d-none d-md-flex justify-content-between flex-wrap"
         style={{
           background: palette.primary,
         }}
       >
         {REGIONS.map((r, i) => (
-          <a
-            href="#"
-            key={i}
-            className="mx-2 text-white text-uppercase small text-decoration-none"
-          >
-            <small>{r}</small>
-          </a>
+          <div className="h-100 region-btn py-2">
+            <a
+              href="#"
+              key={i}
+              className="mx-2 text-white text-uppercase small text-decoration-none"
+            >
+              <small>{r}</small>
+            </a>
+          </div>
         ))}
       </div>
 
